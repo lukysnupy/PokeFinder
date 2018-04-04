eb86a274481f8955e2a68383c908be3009e7445b
Lukáš Růžička <lukas.ruzicka@synetech.cz>
2018-04-04 12:07:07 +0200

    GeoFire added

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Lukáš Růžička on 04.04.18.
+//  Copyright © 2018 Lukáš Růžička. All rights reserved.
+//
+
+#import "GeoFire.h"
--- PokeFinder/ViewController.swift
+++ PokeFinder/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var geoFire: GeoFire!
 
     override func viewDidLoad() {
         super.viewDidLoad()
