9513309b8e82f5e34ebf99fa263ee7ea559935a9
Lukáš Růžička <lukas.ruzicka@synetech.cz>
2018-04-04 12:31:46 +0200

    UI init

---
+++ PokeFinder/Controller/MainVC.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  PokeFinder
+//
+//  Created by Lukáš Růžička on 04.04.18.
+//  Copyright © 2018 Lukáš Růžička. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MainVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    var geoFire: GeoFire!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinareRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        mapView.setRegion(coordinareRegion, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if let loc = userLocation.location {
+            if !mapHasCenteredOnce {
+                centerMapOnLocation(location: loc)
+                mapHasCenteredOnce = true
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+        }
+        
+        return annotationView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func spotPokemon(_ sender: UIButton) {
+    }
+    
+}
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #  PokeFinder
 
-iOS app storing Pokemon locations using Firebase and Apple Maps API.
+iOS app storing Pokemon locations using Firebase, GeoFire and Apple Maps API.
 
 Made with Devslopes courses.
 
